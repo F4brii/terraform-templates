a28b235d321e74ef81a294f39bffc7a0f9da409d
Fabricio Caicedo <48930687+F4brii@users.noreply.github.com>
2025-07-02 12:37:50 -0500

    Update outputs.tf

--- sqs/outputs.tf
+++ sqs/outputs.tf
@@ -1,9 +1,9 @@
-output "sqs_queue_url" {
+output "url" {
   description = "La URL de la cola SQS"
   value       = aws_sqs_queue.this.id
 }
 
-output "sqs_queue_arn" {
+output "arn" {
   description = "El ARN de la cola SQS"
   value       = aws_sqs_queue.this.arn
 }
